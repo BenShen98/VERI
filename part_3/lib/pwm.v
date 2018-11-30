07074fbe49db16e2dd2209ae2ac07d63af4fc999
Ben Shen <shenleyang007@gmail.com>
2018-11-30 17:15:21 +0000

    not test ex11

---
+++ part_3/lib/pwm.v
@@ -0,0 +1,27 @@
+module pwm(clk,data_in,load,pwm_out);
+	
+	input clk;
+	input [9:0] data_in;
+	input load;
+	output pwm_out;
+	
+	reg [9:0] d;
+	reg [9:0] count;
+	reg pwm_out;
+	
+	always @ (posedge clk)
+		if (load == 1'b1)
+			d<= data_in;
+			
+	initial count = 10'b0;
+	
+	always @ (posedge clk) 
+		begin 
+			count<=count + 1'b1;
+			if (count>d)
+				pwm_out<=1'b0;
+			else 
+				pwm_out<=1'b1;
+			end
+endmodule
+		
